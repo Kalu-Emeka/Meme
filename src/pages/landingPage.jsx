b3538faf46e813c573d87e4a7f7354d301952752
Kalu-Emeka <brickkalu@gmail.com>
2025-03-01 19:12:07 +0100

    Edited the page

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -10,6 +10,16 @@ const data = [
     { name: "Early Investors", value: 7.5, color: "#FF5733" }, // Pink
     { name: "Free Supply", value: 7.5, color: "#F39C12" }, // Pink
 ];    
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+// Demo styles, see 'Styles' section below for some notes on use.
+import 'react-accessible-accordion/dist/fancy-example.css';
 function LandingPage () {
     return (
         <>
@@ -32,16 +42,16 @@ function LandingPage () {
                             />
                         </div>
                     </div>
-                    <div className='sm:mt-[40px] md:mt-[20px] lg:mt-0 flex justify-center items-center flex-col'>
+                    <div className='sm:mt-[40px] md:mt-[20px] lg:mt-0 flex justify-center items-center flex-col sm:justify-center md:justify-center lg:justify-start sm:items-center md:items-center lg:items-start'>
                         <h2 className='cartoon-text sm:text-center md:text-center lg:text-left mb-[10px] text-green mt-0 sm:text-[30px] md:text-[30px] lg:text-[40px]'>MEME WORLD ORDER IS NOW LIVE</h2>
-                        <p className='text-[15px] mt-[10px] font-bold text-center sm:text-center md:text-center lg:text-left'>Welcome to $MWOR. We are building a full blow movement on Solana</p>
+                        <p className='text-[15px] mt-[10px] font-bold text-left sm:text-center md:text-center lg:text-left'>Welcome to $MWOR. We are building a full blow movement on Solana</p>
                         <div className='flex'>
-                            <button className='rounded-[70px] mr-[10px] border-black border-2 px-[20px] py-[10px] border mt-[20px] font-bold text-[19px] cursor-pointer bg-[#2eb335]'><a href="https://dexscreener.com/solana/GjeoEzugvhZYHyALgCLcxcLfbB52vRfz8jXFn26oNqsx" target="_blank" rel="noopener noreferrer">Pump.fun</a></button>
+                            <button className='rounded-[70px] mr-[10px] border-black border-2 px-[20px] py-[10px] border mt-[20px] font-bold text-[19px] cursor-pointer bg-[#2eb335]'><a href="https://raydium.io/swap/?inputMint=sol&outputMint=EoRe4xECTe9imaYtwfdmrGoinD2S1N5yMeu1LrjQpump&referrer=BosMQ79Pdj824ywxKtX6sG293QFSRAGC7EvBooZXCboj" target="_blank" rel="noopener noreferrer">Buy now</a></button>
                             <button className='rounded-[70px] border-black border-2 px-[20px] py-[10px] border mt-[20px] font-bold text-[19px] cursor-pointer bg-white'><a href="https://dexscreener.com/solana/GjeoEzugvhZYHyALgCLcxcLfbB52vRfz8jXFn26oNqsx" target="_blank" rel="noopener noreferrer">DexScreener</a></button>
                         </div>
                     </div>
                     <div className='flex justify-center items-center flex-col-reverse mr-[80px] flex-row sm:mr-[20px] md:mr-[20px] lg:mr-[80px]'>
-                        <img className='w-[75%] object-contain' src="/Images/hero.png" alt="" />
+                        <img className='w-[85%] object-contain' src="/Images/hero.png" alt="" />
                     </div>
                 </div>
             </section>
@@ -216,29 +226,28 @@ function LandingPage () {
                 </div>
             </section>
             <section id='tokenomics'>
-                <h2 className='cartoon-text mb-[20px] text-green text-center mt-[20px] mb-[30px]'>Tokenomics</h2>
-                <div className="pr-[80px] rounded-lg flex justify-center items-center sm:pr-[40px]">
-                    <PieChart width={500} height={500}>
+                <h2 className='cartoon-text mb-[20px] text-green text-center mt-[100px] mb-[30px]'>Tokenomics</h2>
+                <div className="rounded-lg flex justify-center items-center">
+                <PieChart width={400} height={400}>
                     <Pie
                         data={data}
                         cx="50%"
                         cy="50%"
-                        outerRadius={200}
+                        outerRadius={140}
                         dataKey="value"
                         label
-                        className='mb-[30px]'
                     >
                         {data.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={entry.color} />
                         ))}
                     </Pie>
                     <Tooltip />
-                    <Legend/>
-                    </PieChart>
+                    <Legend />
+                </PieChart>
                 </div>
             </section>
             <section id="team">
-                <h2 className='cartoon-text mb-[20px] text-green text-center mt-[20px] mb-[30px]'>Team</h2>
+                <h2 className='cartoon-text text-green text-center mt-[120px] mb-[30px]'>Team</h2>
                 <div className='grid grid-cols-3 gap-8 place-items-center px-[20px] sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 sm:px-[40px] md:px-[60px] lg:px-[20px]'>
                     <div className='bg-gray-900 px-[30px] flex justify-center flex-col items-center rounded-xl shadow-md mr-[30px] sm:mr-0 md:mr-0 lg:mr-[30px]'>
                         <img className='w-[50%] mb-[20px] mt-[40px] rounded-[130px]' src="/Images/founder.png" alt="" />
@@ -248,7 +257,7 @@ function LandingPage () {
                     <div className='bg-gray-900 px-[30px] flex justify-center flex-col items-center rounded-xl shadow-md mr-[30px] sm:mr-0 md:mr-0 lg:mr-[30px]'>
                         <img className='w-[50%] mb-[20px] mt-[40px] rounded-[130px]' src="/Images/team1.jpg" alt="" />
                         <h2 className='text-white text-[19px] mb-[20px]'>Glayzz_4T9ne</h2>
-                        <p className='text-[#94a3b8] mb-[40px] w-[70%] text-center'>Community Manager | Graphic Designer</p>
+                        <p className='text-[#94a3b8] mb-[40px] w-[70%] text-center'>Community Manager  Graphic Designer</p>
                     </div>
                     <div className='bg-gray-900 px-[30px] flex justify-center flex-col items-center rounded-xl shadow-md mr-[30px] sm:mr-0 md:mr-0 lg:mr-[30px]'>
                         <img className='w-[50%] mb-[20px] mt-[40px] rounded-[130px]' src="/Images/social.jpg" alt="" />
@@ -257,15 +266,112 @@ function LandingPage () {
                     </div>
                 </div>
             </section>
+            <section id="buy" className='px-[50px]'>
+                <h2 className='cartoon-text mb-[20px] text-green text-center mt-[120px]'>HOW TO BUY</h2>
+                <div className='flex items-center justify-center mt-[50px] sm:block md:block lg:flex'>
+                    <div className='mr-[40px] w-[130%] sm:w-[97%] md:w-[97%] lg:w-[130%]'>
+                        <div className='grid grid-cols-2 gap-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2'>
+                            <div className='bg-gray-900 rounded-md px-[30px] py-[30px]'>
+                                <h2 className='text-white font-bold mb-[20px]'>1. CREATE A WALLET</h2>
+                                <p className='text-white'>Set up a Solana-compatible wallet (e.g., Phantom or Solflare) that supports $MWO. Remember to keep your seed phrase and private keys secure.</p>
+                            </div>
+                            <div className='bg-gray-900 rounded-md px-[20px] py-[20px]'>
+                                <h2 className='text-white font-bold mb-[20px]'>2. Participate in the Fair Launch</h2>
+                                <p className='text-white'>Head over to pump.fun to be part of the fair launch. There’s no presale—everyone obtains $MWO tokens at the same time, ensuring equal opportunity for all.</p>
+                            </div>
+                        </div>
+                        <div className='grid grid-cols-2 gap-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2'>
+                            <div className='bg-gray-900 rounded-md px-[20px] py-[20px] mt-[10px]'>
+                                <h2 className='text-white font-bold mb-[20px]'>3. Stake for Rewards</h2>
+                                <p className='text-white'>Once you have your $MWO tokens, consider staking them to earn additional rewards. Check out our staking page (if applicable) for more information on how to maximize your $MWO holdings.</p>
+                            </div>
+                            <div className='bg-gray-900 rounded-md px-[20px] py-[20px] mt-[10px]'>
+                                <h2 className='text-white font-bold mb-[20px]'>4. You are now a $MWO holder!</h2>
+                                <p className='text-white'>Welcome to the #MWO Community.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='border rounded-lg border-black border-2 sm:mt-[30px] md:mt-[30px] lg:mt-0'>
+                        <img className='' src="/Images/buy.jpg" alt="" />
+                    </div>
+                </div>
+            </section>
+            <section>
+                <h2 className='cartoon-text mb-[20px] text-green text-center mt-[120px]'>FAQS</h2>
+                <div className='px-[50px]'>
+                    <Accordion>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton className='text-[20px] text-white bg-gray-900 px-[40px] py-[20px] rounded-md sm:text-[18px] md:text-[18px] lg:text-[20px]'>
+                                    What is $MWO?
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p className='text-white text-[18px] sm:text-[16px] md:text-[16px] lg:text-[18px]'>
+                                    $MWO is a meme-centric digital asset on Solana, aiming to blend fun and profitability by fostering a spirited community and offering intuitive staking mechanisms.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton className='text-[20px] text-white bg-gray-900 px-[40px] py-[20px] rounded-md sm:text-[18px] md:text-[18px] lg:text-[20px]'>
+                                    Where can I buy $MWO?
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p className='text-white text-[18px] sm:text-[16px] md:text-[16px] lg:text-[18px]'>
+                                    You can purchase $MWO through the fair launch process on pump.fun. Eventually, $MWO may appear on various decentralized and centralized exchanges, offering extra ways to trade or acquire more tokens.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton className='text-[20px] text-white bg-gray-900 px-[40px] py-[20px] rounded-md sm:text-[18px] md:text-[18px] lg:text-[20px]'>
+                                    Is $MWO secure?
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p className='text-white text-[18px] sm:text-[16px] md:text-[16px] lg:text-[18px]'>
+                                    Absolutely. We prioritize security with advanced encryption, frequent audits, and best-practice compliance to protect holders and the overall ecosystem.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton className='text-[20px] text-white bg-gray-900 px-[40px] py-[20px] rounded-md sm:text-[17px] md:text-[17px] lg:text-[20px]'>
+                                    How can I contact the $MWO team?
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p className='text-white text-[18px]  sm:text-[16px] md:text-[16px] lg:text-[18px]'>
+                                    Connect with us on our official social channels—Twitter, Discord, and Telegram. Our support team is ready to assist with any questions or feedback you may have.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                    </Accordion>
+                </div>
+            </section>
             <section className='mx-[40px]'>
-                <h2 className='mb-[20px] text-green-800 text-[20px] mt-[20px] font-bold'>Maintaining Order For Memes On Solana</h2>
-                <p className='text-[15px] mt-[10px] font-bold'>Better speed. Better gains. Same delicious spicy flavor.</p>
-                <div className='flex justify-start space-x-1 mt-[10px] items-center'>
-                    <a href="https://t.me/memeworldorderchat" target="_blank" rel="noopener noreferrer"><img className='w-[50%] object-contain cursor-pointer' src="/Images/tg.png" alt="" /></a>
-                    <a href="https://www.reddit.com/u/mworder2024/s/eJTNkOVjg7" target="_blank" rel="noopener noreferrer"><img className='w-[30%] object-contain cursor-pointer' src="/Images/reddit.png" alt="" /></a>
-                    <a href="https://twitter.com/mworder2024" target="_blank" rel="noopener noreferrer"><img className='w-[20%] object-contain cursor-pointer' src="/Images/x.png" alt="" /></a>
-                    <a href="https://discord.com/invite/H7eCbTZKHu" target="_blank" rel="noopener noreferrer"><img className='w-[40%] object-contain cursor-pointer' src="/Images/discord.png" alt="" /></a>
-                    <a href="http://medium.com/@memeworldorder" target="_blank" rel="noopener noreferrer"><img className='w-[30%] object-contain cursor-pointer' src="/Images/medium.png" alt="" /></a>
+                <div className='grid grid-cols-2 gap-4 place-items-center mt-[20px] sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2'>
+                    <div className='bg-gray-900 px-[20px] rounded-md'>
+                        <h2 className='mb-[20px] text-green-800 text-[20px] mt-[20px] font-bold'>Maintaining Order For Memes On Solana</h2>
+                        <p className='text-[15px] mt-[10px] font-bold text-white'>Better speed. Better gains. Same delicious spicy flavor.</p>
+                        <div className='flex justify-center space-x-1 mt-[10px] items-center mb-[20px] sm:space-x-8 md:space-x-8 lg:space-x-1'>
+                            <a href="https://t.me/memeworldorderchat" target="_blank" rel="noopener noreferrer"><img className='w-[50%] object-contain cursor-pointer sm:w-[98%] md:w-[70%] lg:w-[50%]' src="/Images/tg.png" alt="" /></a>
+                            <a href="https://www.reddit.com/u/mworder2024/s/eJTNkOVjg7" target="_blank" rel="noopener noreferrer"><img className='w-[30%] object-contain cursor-pointer sm:w-[70%] md:w-[40%] lg:w-[30%]' src="/Images/reddit.png" alt="" /></a>
+                            <a href="https://twitter.com/mworder2024" target="_blank" rel="noopener noreferrer"><img className='w-[20%] object-contain cursor-pointer sm:w-[60%] md:w-[40%] lg:w-[20%]' src="/Images/x.png" alt="" /></a>
+                            <a href="https://discord.com/invite/H7eCbTZKHu" target="_blank" rel="noopener noreferrer"><img className='w-[40%] object-contain cursor-pointer sm:w-[80%] md:w-[50%] lg:w-[40%]' src="/Images/discord.png" alt="" /></a>
+                        </div>
+                        <div className='flex justify-center space-x-8 mt-[10px] items-center mb-[20px]'>
+                            <a href="http://medium.com/@memeworldorder" target="_blank" rel="noopener noreferrer"><img className='w-[30%] object-contain cursor-pointer sm:w-[90%] md:w-[50%] lg:w-[40%]' src="/Images/medium.png" alt="" /></a>
+                            <a href="http://medium.com/@memeworldorder" target="_blank" rel="noopener noreferrer"><img className='w-[30%] object-contain cursor-pointer sm:w-[93%] md:w-[50%] lg:w-[40%]' src="/Images/facebook.png" alt="" /></a>
+                            <a href="http://medium.com/@memeworldorder" target="_blank" rel="noopener noreferrer"><img className='w-[30%] object-contain cursor-pointer sm:w-[93%] md:w-[50%] lg:w-[40%]' src="/Images/tik.png" alt="" /></a>
+                            <a href="http://medium.com/@memeworldorder" target="_blank" rel="noopener noreferrer"><img className='w-[30%] object-contain cursor-pointer sm:w-[94%] md:w-[50%] lg:w-[50%]' src="/Images/instagram.png" alt="" /></a>
+                        </div>
+                    </div>
+                    <div>
+                        <img className='w-[70%] sm:w-[90%] md:w-[90%] lg:w-[70%]' src="/Images/hero.png" alt="" />
+                    </div>
                 </div>
             </section>
         </>
